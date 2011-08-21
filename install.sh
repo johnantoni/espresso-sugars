205fdca3c070ffd0c3a265c7893f46cbc8cb9388
John Antoni Griffiths <john@johnantoni.com>
2011-08-21 06:42:23 -0300

    Edited install.sh via GitHub

--- install.sh
+++ install.sh
@@ -12,3 +12,5 @@ git clone https://github.com/fullsailor/HAML.sugar.git
 # sass
 git clone https://github.com/hawx/Sass.sugar.git
 
+# ruby (fork'd one, original no longer maintained)
+git clone https://github.com/hawx/ruby.sugar.git
