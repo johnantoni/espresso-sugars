7ac9272ccee0ee2918aeeaac6cc5de61c03e7f67
John Antoni Griffiths <john@johnantoni.com>
2011-08-21 06:25:46 -0300

    add haml & sass

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+# css3
 git clone https://github.com/minimalweb/CSS3.sugar.git
+
+# html5
 git clone https://github.com/minimalweb/HTML5.sugar.git
+
+# haml
+git clone https://github.com/fullsailor/HAML.sugar.git
+
+# sass
+git clone https://github.com/hawx/Sass.sugar.git
+
